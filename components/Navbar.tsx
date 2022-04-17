d8ef0b7b06f5be71a3e4524e555aad6d683b3c23
tranhuyviet <vietth@msn.com>
2022-04-17 15:15:03 +0300

    Add logout button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,38 @@
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { Avatar, Badge, Divider } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 const Navbar = () => {
   const { data: session, status } = useSession();
@@ -13,7 +45,7 @@ const Navbar = () => {
     );
 
   return (
-    <nav className="h-[64px] bg-green-600 flex items-center space-x-4 text-lg font-semibold text-gray-50 px-4 justify-between">
+    <nav className="h-[64px] bg-indigo-600/80 flex items-center space-x-4 text-lg font-semibold text-gray-50 px-4 justify-between">
       <Link href={"/"}>
         <a>Home</a>
       </Link>
@@ -22,8 +54,25 @@ const Navbar = () => {
           <a>Login</a>
         </Link>
       )}
-      {status === "authenticated" && (
-        <button onClick={() => signOut()}>Log out</button>
+      {status === "authenticated" && session.user && (
+        <div className="flex space-x-4 h-full items-center">
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            variant="dot"
+          >
+            <Avatar
+              src={session.user?.image || undefined}
+              alt={session.user?.name || undefined}
+              sizes="large"
+            />
+          </StyledBadge>
+          <p>{session.user?.name}</p>
+
+          <Divider orientation="vertical" className="!h-8 !bg-gray-200/90" />
+
+          <button onClick={() => signOut()}>Log out</button>
+        </div>
       )}
     </nav>
   );
