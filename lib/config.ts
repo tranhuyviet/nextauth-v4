e7123f3ac53d74dca064e80a7214e417e2375553
tranhuyviet <vietth@msn.com>
2022-04-18 15:12:42 +0300

    Fix deploy 3

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 const apiUrl =
   process.env.NODE_ENV === "production"
-    ? (process.env.API_URL as string) + "/api"
+    ? "https://nextauth-v4.vercel.app/api"
     : "http://localhost:3000/api";
 
 export { apiUrl };
